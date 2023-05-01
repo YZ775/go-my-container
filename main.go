6d416cd8917952ebfa13c67bfbcd0e3e0d130595
YZ775 <yuzuki725.m@gmail.com>
2023-05-01 21:27:57 +0900

    add loading docker image function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -8,22 +9,34 @@ import (
 	"syscall"
 )
 
+type DockerManifest []struct {
+	Config   string   `json:"Config"`
+	RepoTags []string `json:"RepoTags"`
+	Layers   []string `json:Layers`
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		panic("not enough argument")
 	}
+	var err error
 	switch os.Args[1] {
 	case "run":
-		run()
+		err = run()
 	case "child":
-		child()
+		err = child()
+	case "load":
+		err = load_image("nginx.tar")
 	default:
 		panic("invalid command")
 	}
+	if err != nil {
+		panic(err)
+	}
 }
 
-func run() {
-	// fmt.Printf("Running %v as PID %d \n", os.Args[2:], os.Getpid())
+func run() error {
+	fmt.Printf("Running main process as PID %d \n", os.Getpid())
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -31,32 +44,49 @@ func run() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
-	check_err(cmd.Run())
-
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func child() {
-	fmt.Printf("Running %v as PID %d \n", os.Args[2:], os.Getpid())
+func child() error {
+	fmt.Printf("[Child]Running %v as PID %d \n", os.Args[2:], os.Getpid())
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	check_err(set_cgroup_setting(os.Getegid(), "1000", "10000", "100m"))
-
-	check_err(syscall.Mount("/dev", "/home/ubuntu/Home/tech/go-my-container/rootfs/dev", "devtmpfs", syscall.MS_BIND, ""))
-	check_err(syscall.Chroot("/home/ubuntu/Home/tech/go-my-container/rootfs"))
-	check_err(os.Chdir("/"))
-	syscall.Mount("proc", "/proc", "proc", 0, "")
-
-	check_err(syscall.Sethostname([]byte("container")))
-	check_err(cmd.Run())
-
-}
-
-func check_err(err error) {
+	err := set_cgroup_setting(os.Getegid(), "1000", "10000", "100m")
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	err = syscall.Mount("/dev", "/home/ubuntu/Home/tech/go-my-container/rootfs2/dev", "devtmpfs", syscall.MS_BIND, "")
+	if err != nil {
+		return err
+	}
+	err = syscall.Chroot("/home/ubuntu/Home/tech/go-my-container/rootfs2")
+	if err != nil {
+		return err
+	}
+	err = os.Chdir("/")
+	if err != nil {
+		return err
+	}
+	syscall.Mount("proc", "/proc", "proc", 0, "")
+	if err != nil {
+		return err
+	}
+	err = syscall.Sethostname([]byte("container"))
+	if err != nil {
+		return err
+	}
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func set_cgroup_setting(pid int, cpu_quota string, cpu_period string, memory_max_mb string) error {
@@ -75,6 +105,31 @@ func set_cgroup_setting(pid int, cpu_quota string, cpu_period string, memory_max
 	err = ioutil.WriteFile("/sys/fs/cgroup/my-container/memory.max", []byte(fmt.Sprintf("%s", memory_max_mb)), 700)
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func load_image(filename string) error {
+	fmt.Print(fmt.Sprintf("./image/%s\n", filename))
+	cmd := exec.Command("tar", "-xvf", fmt.Sprintf("./image/%s", filename), "-C", "./image/archive")
+	err := cmd.Run()
+	if err != nil {
+		return (err)
+	}
+	byteArray, err := ioutil.ReadFile("./image/archive/manifest.json")
+	if err != nil {
+		return (err)
+	}
+	manifest := make(DockerManifest, 0)
+	err = json.Unmarshal(byteArray, &manifest)
+	if err != nil {
+		return (err)
+	}
+	top_layer := manifest[0].Layers[0]
+	cmd = exec.Command("tar", "-xvf", fmt.Sprintf("./image/archive/%s", top_layer), "-C", "rootfs2")
+	err = cmd.Run()
+	if err != nil {
+		return (err)
 	}
 	return nil
 }
