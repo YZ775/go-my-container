0c21d58137f66db851943d1bcdfdd9dfd47d1f55
YZ775 <yuzuki725.m@gmail.com>
2023-05-01 20:20:00 +0900

    add devtmpfs, cgroup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/YZ775/go-my-container
 
 go 1.16
+
+require github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"syscall"
@@ -28,7 +29,7 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 	check_err(cmd.Run())
 
@@ -40,15 +41,14 @@ func child() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	check_err(set_cgroup_setting(os.Getegid(), "1000", "10000", "100m"))
 
-	current_path, _ := os.Getwd()
-	new_root := fmt.Sprintf("NEW ROOT: %s/rootfs\n", current_path)
-	fmt.Print(new_root)
-	// check_err(syscall.Chroot(new_root))
+	check_err(syscall.Mount("/dev", "/home/ubuntu/Home/tech/go-my-container/rootfs/dev", "devtmpfs", syscall.MS_BIND, ""))
 	check_err(syscall.Chroot("/home/ubuntu/Home/tech/go-my-container/rootfs"))
-
 	check_err(os.Chdir("/"))
-	syscall.Mount("proc", "proc", "proc", 0, "")
+	syscall.Mount("proc", "/proc", "proc", 0, "")
+
+	check_err(syscall.Sethostname([]byte("container")))
 	check_err(cmd.Run())
 
 }
@@ -57,4 +57,24 @@ func check_err(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func set_cgroup_setting(pid int, cpu_quota string, cpu_period string, memory_max_mb string) error {
+	err := os.MkdirAll("/sys/fs/cgroup/my-container/", 700)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("/sys/fs/cgroup/my-container/cgroup.procs", []byte(fmt.Sprintf("%d", pid)), 700)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("/sys/fs/cgroup/my-container/cpu.max", []byte(fmt.Sprintf("%s %s", cpu_quota, cpu_period)), 700)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("/sys/fs/cgroup/my-container/memory.max", []byte(fmt.Sprintf("%s", memory_max_mb)), 700)
+	if err != nil {
+		return err
+	}
+	return nil
 }
